cd24d08f07de13d2028fbc8871e1c15175437697
Iamneo <techadmin@iamneo.ai>
2023-10-03 08:04:57 +0000

    Backup Commit

---
+++ src/model/Medicine.java
@@ -0,0 +1,9 @@
+package com.pharmacy.model;
+public class Medicine
+{
+    private int medicineId;
+    private String medicineName;
+    private float price;
+    private String description;
+    private int quantity;
+}
