c02015e39f67b80345f25ac9310d0fd6f6d1409e
Iamneo <techadmin@iamneo.ai>
2023-10-03 08:09:58 +0000

    Backup Commit

--- src/controller/MedicineController.java
+++ src/controller/MedicineController.java
@@ -1,1 +1,17 @@
 package com.pharmacy.controller;
+import com.pharmacy.model.Medicine;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/medicines")
+public class MedicineController
+{
+    private Map< Integer, Medicine> medicineMap=new HashMap<>();
+    @postMapping
+    public boolean addMedicine(@RequestBody Medicine medicine)
+    {
+        if(medicine !=null && medicine)
+    }
+}
